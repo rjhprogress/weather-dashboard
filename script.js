898a589328638fb2bf87e1162a5ce04fd742cbeb
rjhprogress <68923976+rjhprogress@users.noreply.github.com>
2020-10-08 04:52:03 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $('#search-city').on("click", function () {
 var searchValue = $("#search-city").val();
 
   // Get API  & API Key
-  var queryURL =  "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=appid=924064a95edd0c1c942e4fa79c7d8039";
+  var queryURL =  "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&APPID=924064a95edd0c1c942e4fa79c7d8039";
 
 
   //Ajax GET request to our queryURL 
